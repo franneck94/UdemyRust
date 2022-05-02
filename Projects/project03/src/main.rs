8ff7f6c3ce5db83d91d6307e122e9f1b3d1c1a7e
franneck94 <schaffranek.jan@web.de>
2022-05-02 15:17:38 +0200

    up

--- Projects/project03/src/main.rs
+++ Projects/project03/src/main.rs
@@ -1,3 +1,23 @@
+use std::num::ParseIntError;
+
+pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
+    let processing_fee = 1;
+    let cost_per_item = 5;
+    let qty = item_quantity.parse::<i32>()?;
+
+    Ok(qty * cost_per_item + processing_fee)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut tokens = 100;
+    let pretend_user_input = "8";
+
+    let cost = total_cost(pretend_user_input).unwrap_or(0);
+
+    if cost > tokens {
+        println!("You can't afford that many!");
+    } else {
+        tokens -= cost;
+        println!("You now have {} tokens.", tokens);
+    }
 }
--- Projects/project06/src/main.rs
+++ Projects/project06/src/main.rs
@@ -1,3 +1,27 @@
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
+struct JobStatus {
+    jobs_completed: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let status = Arc::new(Mutex::new(JobStatus { jobs_completed: 0 }));
+    let status_cloned = Arc::clone(&status);
+
+    thread::spawn(move || {
+        let status = Arc::clone(&status);
+        let mut status_num = status.lock().unwrap();
+
+        for _ in 0..10 {
+            thread::sleep(Duration::from_millis(250));
+            status_num.jobs_completed += 1;
+        }
+    });
+
+    while status_cloned.lock().unwrap().jobs_completed < 10 {
+        println!("waiting... ");
+        thread::sleep(Duration::from_millis(500));
+    }
 }
