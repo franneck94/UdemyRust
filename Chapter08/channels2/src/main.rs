b156b4e677949a5f18410cbbcf75f646e6c2ca0b
franneck94 <schaffranek.jan@web.de>
2024-03-28 07:01:10 +0100

    up

--- Chapter08/channels2/src/main.rs
+++ Chapter08/channels2/src/main.rs
@@ -1,10 +1,9 @@
-// Do not communicate by sharing memory; instead, share memory by communicating
-// A channel is a general programming concept by which data is sent from one thread to another.
 use std::sync::mpsc;
 use std::thread;
 
 fn main() {
     let (transmitter1, reciever1) = mpsc::channel();
+    let transmitter2 = transmitter1.clone();
 
     thread::spawn(move || {
         let msg = String::from("Hello From Thread T1");
@@ -16,10 +15,15 @@ fn main() {
         transmitter1.send(msg).unwrap();
     });
 
-    // let recieved_msg1 = reciever1.recv().unwrap();
-    // println!("Recieved1: {recieved_msg1}");
-    // let recieved_msg2 = reciever1.recv().unwrap();
-    // println!("Recieved2: {recieved_msg2}");
+    thread::spawn(move || {
+        let msg = String::from("Hello From Thread T2");
+        transmitter2.send(msg).unwrap();
+
+        // DO SOME STUFF
+
+        let msg = String::from("Goodbye From Thread T2");
+        transmitter2.send(msg).unwrap();
+    });
 
     for recieved_msg in reciever1.iter() {
         println!("Recieved: {recieved_msg}");
